43bdd56769ff52730593ac6659bf383b84e8c32e
swati-anchatageri <103919084+swati-anchatageri@users.noreply.github.com>
2022-05-10 16:31:50 +0530

    first commit

---
+++ src/main/java/com/example/demo/test.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class test {
+    @RequestMapping("/")
+    @ResponseBody
+    public String hello() {
+        return "Hello Spring Boot";
+    }
+}
